ea32aac8ffd4fbbc1573754736a425dc03f49cb4
Breno Farias da Silva <breno.vicio78@gmail.com>
2025-10-07 08:09:15 -0300

    FEAT: Adding the verify_filepath_exists function to dataset_descriptor.py

--- main.py
+++ main.py
@@ -42,6 +42,18 @@ def verbose_output(true_string="", false_string=""):
    elif false_string != "": # If the false_string is set
       print(false_string) # Output the false statement string
 
+def verify_filepath_exists(filepath):
+   """
+   Verify if a file or folder exists at the specified path.
+
+   :param filepath: Path to the file or folder
+   :return: True if the file or folder exists, False otherwise
+   """
+
+   verbose_output(f"{BackgroundColors.GREEN}Verifying if the file or folder exists at the path: {BackgroundColors.CYAN}{filepath}{Style.RESET_ALL}") # Output the verbose message
+
+   return os.path.exists(filepath) # Return True if the file or folder exists, False otherwise
+
 def main():
    """
    Main function.
